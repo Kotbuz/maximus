473a3163d9d68b632be82d4042c22cb5b58b7cb7
Kotbuz <efimisb@gmail.com>
2023-07-21 21:50:04 +0700

    Ну хз

--- PY_Projango/DjangoProject/app_lesson_4/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.http import HttpResponse
-from .views import index
-urlpatterns=[
-    path('', index)
-]
--- lesson_1.py
+++ lesson_1.py
@@ -1,10 +1,30 @@
-a=input()
-A=[]
-B=[]
-for i in a:
-    A.append(i)
-B=A[::-1]
-if A==B:
-    print(True)
-else:
-    print(False)
+#N=4, M=5, ИТОГО: 20 повторений
+#Сложность: O(N*M)
+def strcounter(stroka):
+    for sym in set(stroka): #4 значения - значит - 4 повторения
+        counter=0
+        for sub_sym in stroka: #5 значений в stroka - 5 повт.
+            if sym==sub_sym:
+                counter+=1
+        print(sym, counter)
+#Переделаем в Сложность: O(N)
+def strcounter_new(stroka):
+    syms_counter={}
+    for sym in stroka:
+        syms_counter[sym]=syms_counter.get(sym, 0)+1
+    print(syms_counter)
+
+stroka='aabcd'
+print(set(stroka))
+strcounter(stroka)
+
+strcounter_new(stroka)
+
+#ДЗ:
+def is_palindrome(string):
+    return string == string[::-1]
+
+print(is_palindrome('лепсспел'))
+#
+#dsgdfhd
+
