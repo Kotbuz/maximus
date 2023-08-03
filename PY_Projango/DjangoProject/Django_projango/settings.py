be4a723b208f1e5ab58fe64485fff90f4c668e81
Kotbuz <efimisb@gmail.com>
2023-08-03 23:15:51 +0700

    Ну хз

--- PY_Projango/DjangoProject/Django_projango/settings.py
+++ PY_Projango/DjangoProject/Django_projango/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
