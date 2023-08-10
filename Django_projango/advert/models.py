74172c4405dd69a5e21c78f89f3da677019e7a04
Kotbuz <efimisb@gmail.com>
2023-08-10 19:07:04 +0700

    Дз

---
+++ Django_projango/advert/models.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+from django.utils.html import format_html
+
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
--- PY_Projango/DjangoProject/advert/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import index, top_sellers
-
-urlpatterns = [
-    path('', index),
-    path('top-sellers', top_sellers)
-]
